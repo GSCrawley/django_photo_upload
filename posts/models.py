18e52969547faa4f583b1aff51f51c6d013ba400
GSCrawley <gscrawley@gmail.com>
2020-05-13 19:29:33 -0700

    added posts app

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.TextField()
+    cover = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
