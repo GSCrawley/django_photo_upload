0110dcc0c1e4728b79fb39b1a0e3a1214fa8e667
GSCrawley <gscrawley@gmail.com>
2020-05-13 21:52:45 -0700

    modified settings to handle media files

--- insta_project/settings.py
+++ insta_project/settings.py
@@ -52,9 +52,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'insta_project.urls'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -122,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static'),
+]
+ 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
--- insta_project/urls.py
+++ insta_project/urls.py
@@ -17,14 +17,12 @@ from django.contrib import admin
 from django.conf import settings # new
 from django.urls import path, include # new
 from django.conf.urls.static import static # new
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')), # new
     path('accounts/', include('django.contrib.auth.urls')),
 
-]
-
-if settings.DEBUG: # new
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
