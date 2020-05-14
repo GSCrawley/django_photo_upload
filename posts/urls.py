1672c57e596b455f9e5042a7600277c95b0235b9
GSCrawley <gscrawley@gmail.com>
2020-05-13 20:35:35 -0700

    created post page

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, CreatePostView 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('post/', CreatePostView.as_view(), name='add_post') # new
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy 
+from .forms import PostForm
 from .models import Post
 
 
 class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
+
+class CreatePostView(CreateView): # new
+    model = Post
+    form_class = PostForm
+    template_name = 'post.html'
+    success_url = reverse_lazy('home')
