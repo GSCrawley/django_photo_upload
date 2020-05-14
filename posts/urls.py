79e60dd7170b531bbdd5ab9d440354692bf40569
GSCrawley <gscrawley@gmail.com>
2020-05-14 03:58:24 -0700

    added registration app

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
-from .views import HomePageView, CreatePostView 
+from .views import HomePageView, CreatePostView
+from . import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('post/', CreatePostView.as_view(), name='add_post') # new
+    path('post/', CreatePostView.as_view(), name='add_post'), # new
+    path('login/', views.login, name='insta_login'),
+    path('logout/', views.logout, name='insta_logout'),
+    path('admin_page/', views.admin_page, name='admin_page'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy 
 from .forms import PostForm
 from .models import Post
+from django.contrib import auth, messages
 
 
 class HomePageView(ListView):
@@ -14,3 +15,35 @@ class CreatePostView(CreateView): # new
     form_class = PostForm
     template_name = 'post.html'
     success_url = reverse_lazy('home')
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('admin_page')
+ 
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+ 
+        if user is not None:
+            # correct username and password login the user
+            auth.login(request, user)
+            return redirect('admin_page')
+ 
+        else:
+            messages.error(request, 'Error wrong username/password')
+ 
+    return render(request, 'posts/login.html')
+ 
+ 
+def logout(request):
+    auth.logout(request)
+    return render(request,'posts/logout.html')
+ 
+ 
+def admin_page(request):
+    if not request.user.is_authenticated:
+        return redirect('insta_login')
+ 
+    return render(request, 'posts/admin_page.html')
